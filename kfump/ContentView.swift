a389afbe205e797b96b5366b6309b4a66ef362a1
Mehedi Hasan <mehedi.hasan@azolutionse.com>
2024-03-27 23:29:39 +0600

    Initial Commit

---
+++ kfump/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  kfump
+//
+//  Created by Mehedi Hasan on 27/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ kfump/kfumpApp.swift
@@ -0,0 +1,17 @@
+//
+//  kfumpApp.swift
+//  kfump
+//
+//  Created by Mehedi Hasan on 27/3/24.
+//
+
+import SwiftUI
+
+@main
+struct kfumpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
